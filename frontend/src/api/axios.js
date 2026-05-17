a4225dfaf4ee0fdde3862df8ec7c1ac63b309051
arushi <arushi@LAPTOP-CI0BI9MF.com>
2026-05-17 14:43:20 +0530

    feat: update API URL to Railway

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://padhai-production.up.railway.app/api'
 })
 
 api.interceptors.request.use((config) => {
@@ -13,3 +13,9 @@ api.interceptors.request.use((config) => {
 })
 
 export default api
+
+
+cd C:\Users\arushi\OneDrive\Desktop\My_Startup
+git add .
+git commit -m "feat: update API URL to Railway"
+git push
