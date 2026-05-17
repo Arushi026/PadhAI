85f67d5a11c8e1ef60e5931aa81c42744a77ede0
arushi <arushi@LAPTOP-CI0BI9MF.com>
2026-05-17 15:24:56 +0530

    fix axios final

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,11 @@
 import axios from 'axios'
-
 const api = axios.create({
   baseURL: 'https://padhai-production.up.railway.app/api'
 })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if(token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
-
 export default api
-
-
-cd C:\Users\arushi\OneDrive\Desktop\My_Startup
-git add .
-git commit -m "fix: force redeploy"
-git push
