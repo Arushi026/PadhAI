9f025d44c7a437082ceafa8ff3488f2d53baa7a9
arushi <arushi@LAPTOP-CI0BI9MF.com>
2026-06-06 11:58:39 +0530

    fix: update API URL to Render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-  baseURL: 'https://padhai-production.up.railway.app/api'
+  baseURL: 'https://padhai-uk5w.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
